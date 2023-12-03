7cba405edd1aa08bfeed02a179685254d879cd43
_boy4uk <116024276+oleksiyboichuk@users.noreply.github.com>
2023-12-03 16:50:26 +0200

    add all changes

---
+++ src/Tours.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Tour } from './Tour';
+
+export const Tours = ({ tours, removeTour }) => {
+    return <section>
+        <div className='title'>
+            <h2>Our Tours</h2>
+            <div className='underline' />
+        </div>
+        <div>
+            {tours.map((tour) => {
+                return <Tour key={tour.id} {...tour} removeTour={removeTour} />
+            })}
+        </div>
+    </section>
+}
